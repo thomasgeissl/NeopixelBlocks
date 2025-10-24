53855d8cf6ef77971586b9f0e2aa5cb1068b3568
Thomas Geissl <thomas.geissl@gmail.com>
2025-10-24 13:11:16 +0200

    no unsed var

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,5 @@
 import { Box } from "@mui/material";
-import Header from "./components/Header";
+// import Header from "./components/Header";
 import BlocklyEditor from "./components/BlocklyEditor";
 import useAppStore from "./stores/app";
 import Settings from "./components/Settings";
